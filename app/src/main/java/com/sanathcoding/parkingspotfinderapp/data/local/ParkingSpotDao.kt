fa2c9c6299fdc89d89513584e5429f35bb8fcd13
Sanath <sanath@entrusttitle.com>
2023-01-30 13:39:34 +0530

    Added Data layer with entity and Dao

---
+++ app/src/main/java/com/sanathcoding/parkingspotfinderapp/data/local/ParkingSpotDao.kt
@@ -0,0 +1,18 @@
+package com.sanathcoding.parkingspotfinderapp.data.local
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ParkingSpotDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertParkingSpot(spot: ParkingSpotEntity)
+
+    @Delete
+    suspend fun deleteParkingSpot(spot: ParkingSpotEntity)
+
+    @Query("SELECT * FROM parkingspotentity")
+    fun getParkingSpots(): Flow<List<ParkingSpotEntity>>
+
+}
